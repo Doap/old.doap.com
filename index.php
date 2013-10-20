480401d072382adf18cf584adf0b388148dc026f
doapydave <dave@doap.com>
2013-10-20 02:49:20 +0000

    some basic stuff for the root dir

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Doap PHP App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <style>body {margin-top: 40px; background-color: #333;}</style>
+        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="hero-unit">
+                <h1>Doap PHP App</h1>
+                <h2>Sweet!</h2>
+                <p>The Doap PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;Cloud.</p>
+                <p>We are running PHP version <?php echo phpversion(); ?>.</p>
+                
+                <div style="max-width:960px;">
+   <div style="position:relative;float:right;margin-right: 10px;width:168px;">
+    <img style="position:relative;float:right;margin-right: 10px;" alt="doapy128sq" src="http://doap.com/wp-content/uploads/2013/05/doapy128sq.png" width="168" height="168">
+   </div>
+   
+    <h2>DevOps and Platforms</h2>
+    <P>Welcome to another Doap website:</p>
+      <ul>
+        <li>Repo is here: <a href="https://github.com/Doap/doap-php-simple-app.git">GitHub doap-wordpress-app
+</a></a></li>
+        <li>Click <a href="info.php">here</a> to try some php.</li>
+        <li><a href=http://50.112.224.71/>http://50.112.224.71/</a></li>
+        <li><a href=mysql://54.244.200.191/>mysql://54.244.200.191/</a></li>
+
+
+</ul>
+    </div>
+            </div>
+        </div>
+
+
+ 
+    
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
